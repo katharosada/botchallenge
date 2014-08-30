ae93235c13cf0dd9b6bf95a49a06183b73a6e49f
Katie Bell <katie@katharos.id.au>
2014-08-30 18:38:52 +0200

    Add basic client code.

---
+++ client/examplebot.py
@@ -0,0 +1,17 @@
+
+from client import *
+
+robot = Robot()
+i = 1
+
+square = [Dir.LEFT, Dir.UP, Dir.RIGHT, Dir.DOWN]
+
+while True:
+  for dir in square:
+    for i in range(4):
+      success = robot.move(dir)
+      if not success:
+        print "Failed to move", dir
+
+
+
