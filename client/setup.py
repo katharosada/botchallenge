6a99afde5829694f4b609536fdac296b66c1fbe3
Katie Bell <katie@katharos.id.au>
2014-09-12 13:38:01 +0200

    Release version 1.1 (with fixes for placing blocks and inventory)

--- client/setup.py
+++ client/setup.py
@@ -21,7 +21,7 @@ place blocks.
 
 setup(
     name="botchallenge",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     install_requires=['greenlet'],
     author="Katie Bell",
