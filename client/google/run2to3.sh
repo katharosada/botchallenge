581920cf4e586dcd8b325af013861b0e8e3d6831
Benjamin Gorman <8076bgorman@gmail.com>
2014-12-31 18:01:41 +0000

    Revert client to version from master branch

--- client/google/run2to3.sh
+++ client/google/run2to3.sh
@@ -4,6 +4,13 @@ protobuf/internal/cpp_message.py \
 protobuf/internal/decoder.py \
 protobuf/internal/python_message.py \
 protobuf/internal/type_checkers.py \
+protobuf/internal/message_factory_test.py \
+protobuf/internal/message_factory_python_test.py \
+protobuf/internal/message_python_test.py \
+protobuf/internal/message_test.py \
+protobuf/internal/reflection_test.py \
+protobuf/internal/test_util.py \
+protobuf/internal/text_format_test.py \
 protobuf/message_factory.py \
 protobuf/text_encoding.py \
 protobuf/text_format.py
